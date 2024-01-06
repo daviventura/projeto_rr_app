22f63419ef0e480ffbe83d0320203248e71d8775
daviventura <155470809+daviventura@users.noreply.github.com>
2024-01-05 23:38:20 -0300

    Add files via upload

---
+++ app_rr.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import streamlit as st
+import webbrowser as wb
+
+
+foto_file=open('img\BAURU.jpg','rb').read()
+
+df=pd.read_excel('produtos_rr.xlsx')
+df.head(1)
+
+tipos=df['Tipo'].unique()
+tipo=st.selectbox(label='Combo, Salgada ou Copinho?',
+                  options=tipos)
+df_tipo=df[df['Tipo']==tipo]
+
+
+produtos=df_tipo['Produto'].unique()
+produto=st.selectbox('Qual sabor você quer?',
+                     options=produtos)
+df_produto=df_tipo[df_tipo['Produto']==produto].iloc[0]
+
+link_produto=df_produto['links']
+
+st.image(df_produto['fotos'])
+
+st.title(df_produto['Produto'])
+
+st.markdown(df_produto['descrição'])
+
+if st.button(label='Ver no cardápio',type='secondary'):
+    wb.open(link_produto)
+
+st.progress(df_produto['Preferência dos Clientes'],
+            text='Quanto essa esfiha é pedida no nosso cardápio')
+
+col1,col2,col3=st.columns(3)
+
+
+col1.metric(label='Preço',value=df_produto['Preço'])
+col2.metric(label='Melhor Preço',value=df_produto['Melhor Preço'])
+
+col3.subheader(f"Melhor dia pra compra : {df_produto['Melhor Dia pra Compra']}")
+
+st.sidebar.image('img\RR Esfihas - Logotipo A.png')
+
+if st.sidebar.button(label='Cardápio Digital',type='primary'):
+    wb.open('https://app.cardapioweb.com/rr_esfihas')
