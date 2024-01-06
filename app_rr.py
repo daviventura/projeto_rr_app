44bdf5391e2364af0295f6596b271749405ca1da
daviventura <155470809+daviventura@users.noreply.github.com>
2024-01-06 20:36:11 -0300

    Update app_rr.py

--- app_rr.py
+++ app_rr.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import webbrowser as wb
 
-with open('assets\style.css') as f:
+with open('assets/style.css') as f:
     st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 
 df=pd.read_excel('produtos_rr.xlsx')
